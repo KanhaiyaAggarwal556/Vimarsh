fe1b3a95890e95863a9bc0d9c574acbb8d64c77c
Kanhainya Agrawal <kanhaiaggarwal@gmail.com>
2025-08-12 16:33:37 +0530

    Change the cors system

--- src/components/Post/view_post/Single_Post/services/API.js
+++ src/components/Post/view_post/Single_Post/services/API.js
@@ -1,6 +1,6 @@
 // services/api.js
 const API_BASE_URL =
-   `${import.meta.env.VITE_API_BASE_URL}/api`;
+   `${import.meta.env.VITE_API_BASE_URL}`;
 
 // API service for post interactions
 export const postAPI = {
